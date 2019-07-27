9331ebe0b7952337aa077b65d21bd774d2df1348
presw <pmwhitehall@gmail.com>
2019-07-26 22:07:59 -0700

    Moved username click handler to messagesView

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -4,15 +4,10 @@ var MessagesView = {
 
   initialize: function() {
     this.render();
-
   },
 
   render: function() {
     Parse.readAll((data) => {
-      // Alt for loop for testing
-      // for (var i = 15; i < data.results.length; i++) {
-      //   this.renderMessage(data.results[i]);
-      // }
       for (var val of data.results) {
         if (!val.text.includes('<script>')) {
           if (!val.username) {
@@ -30,7 +25,9 @@ var MessagesView = {
   renderMessage: function(message) {
 
     var messageObject = MessageView.render(message);
-
     $('#chats').append(messageObject);
   }
 };
+
+// Click handlers:
+$('#chats').on('click', '.username', function() {Friends.toggleStatus();});
